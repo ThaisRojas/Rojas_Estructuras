23cd81b1eb616768de2d7dd3756e4e8935b7d863
Thais Rojas <thais.rojas@udla.edu.ec>
2024-01-16 15:28:44 -0500

    prototipo de funciones

--- estructuras.c
+++ estructuras.c
@@ -9,3 +9,34 @@ struct alumno
     char carrera[50];
     float promedio;
 };
+// Prototipo de funciones
+void ingresarDatos(struct alumno *estudiante);
+void mostrarDatos(const struct alumno *estudiante);
+
+int main()
+{
+    int n;
+
+    // Solicita al usuario el número de alumnos
+    printf("Ingrese el número de alumnos: ");
+    scanf("%d", &n);
+
+    // Declara un arreglo de estructuras para almacenar los datos de los alumnos
+    struct alumno estudiantes[n];
+
+    for (int i = 0; i < n; ++i)
+    {
+        printf("\nIngrese datos para el estudiante %d:\n", i + 1);
+        ingresarDatos(&estudiantes[i]);
+    }
+
+    // Muestra los datos de todos los alumnos
+    printf("\nDatos ingresados de los alumnos:\n");
+    for (int i = 0; i < n; ++i)
+    {
+        printf("\nDatos del estudiante %d:\n", i + 1);
+        mostrarDatos(&estudiantes[i]);
+    }
+
+    return 0;
+}
